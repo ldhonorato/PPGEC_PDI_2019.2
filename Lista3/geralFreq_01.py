c05b51efe6bcff7e735be1815549533f8614e352
Leandro Honorato <ld.honorato@gmail.com>
2019-09-05 21:35:03 -0300

    adicionando lpf seguido de  GLPF

--- Lista3/geralFreq_01.py
+++ Lista3/geralFreq_01.py
@@ -11,7 +11,7 @@ imgPath = "images/geral_01.png"
 originalImg = np.array(pil.open(imgPath)).astype(np.int16)
 
 
-for d0 in (25, 50, 100, 150, 200):
+for d0 in (25, 50, 100, 150, 180, 200):
     filtrosFreq.aplica(imgPath, d0, filtrosFreq.LPF, "lpf_"+str(d0))
     filtrosFreq.aplica(imgPath, d0, filtrosFreq.GLPF, "GLPF_"+str(d0))
     filtrosFreq.aplica(imgPath, d0, filtrosFreq.BLPF, "BLPF_n2_"+str(d0), n=2)
@@ -19,3 +19,4 @@ for d0 in (25, 50, 100, 150, 200):
 filtrosFreq.aplica("geral_01_BLPF_n2_200.png", 200, filtrosFreq.GLPF, "GLPF_200")
 filtrosFreq.aplica("geral_01_BLPF_n2_200.png", 300, filtrosFreq.GLPF, "GLPF_300")
 filtrosFreq.aplica("geral_01_BLPF_n2_200.png", 300, filtrosFreq.LPF, "LPF_300")
+filtrosFreq.aplica("geral_01_lpf_180.png", 100, filtrosFreq.GLPF, "GLPF_100")
